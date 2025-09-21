102957dd60cbcc9d5727caeed2d62bf1293c8575
shrayasandhir <ssandhir@unc.edu>
2025-09-21 17:42:10 -0400

    Update install.R

--- install.R
+++ install.R
@@ -3,6 +3,7 @@ install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
 install.packages("skimr", dependencies = TRUE)
+install.packages("Rtsne", dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
 
